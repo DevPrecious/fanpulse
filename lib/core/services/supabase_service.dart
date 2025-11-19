3e0bb8cabf54e1439860814d45c57d0152a51682
Devprecious <oladelep77@gmail.com>
2025-11-19 17:13:32 +0100

    [add] Add

--- lib/core/services/supabase_service.dart
+++ lib/core/services/supabase_service.dart
@@ -8,9 +8,8 @@ class SupabaseService extends GetxService {
 
   Future<SupabaseService> init() async {
     // TODO: Replace with your actual Supabase URL and Anon Key
-    const supabaseUrl = 'https://zbeyqpkdymwiwxjutzxh.supabase.co';
-    const supabaseAnonKey =
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpiZXlxcGtkeW13aXd4anV0enhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM1MTQxNjksImV4cCI6MjA3OTA5MDE2OX0._e8Rrio1bfbeiv6gGK7p6jmqlQ5w7xtUo7Awfb1IH64';
+    const supabaseUrl = '';
+    const supabaseAnonKey = '';
 
     await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
 
